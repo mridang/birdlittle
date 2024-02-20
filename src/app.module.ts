3a4de08c065e15868ba78e3babd212c59263a195
Mridang Agarwalla <mridang@nosto.com>
2024-02-20 18:09:36 +0700

    Registered the HTTP-module so Terminus can make connectivity checks

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TerminusModule } from '@nestjs/terminus';
 import { FinchModule } from './services/finch/finch.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     FinchModule,
+    HttpModule,
     TerminusModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
