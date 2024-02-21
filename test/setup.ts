a860fe7298b23da01f88badf361ab75f98665ae8
Mridang Agarwalla <mridang@nosto.com>
2024-02-21 09:03:55 +0700

    Added logic to debug why the AWS_REGION isn't passed through

--- test/setup.ts
+++ test/setup.ts
@@ -11,6 +11,7 @@ declare global {
 config();
 // this allows test containers to work in build pipeline
 process.env.TESTCONTAINERS_RYUK_DISABLED = 'true';
+console.log(process.env);
 
 // noinspection JSUnusedGlobalSymbols
 export default async function setup(): Promise<void> {
