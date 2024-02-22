81d28fe56fc579b5d6e6a2f2411da13e628da22a
Mridang Agarwalla <mridang@nosto.com>
2024-02-22 15:36:18 +0700

    Added tests for the webhook controller

--- test/services/finch/webhook.controller.test.ts
+++ test/services/finch/webhook.controller.test.ts
@@ -63,4 +63,92 @@ describe('webhook.controller test', () => {
         });
       });
   });
+
+  it('should return 400 without x-github-delivery header', async () => {
+    const webhookSecret = await testModule.app
+      .get(GithubConfig)
+      .getSecret(secretName);
+    const payload = {
+      action: 'opened',
+      issue: { number: 1 },
+      repository: {
+        id: 123,
+        name: 'Hello-World',
+        full_name: 'octocat/Hello-World',
+      },
+      sender: { login: 'octocat' },
+    };
+    const signature = createHmac('sha256', webhookSecret.secret)
+      .update(JSON.stringify(payload))
+      .digest('hex');
+
+    return request(testModule.app.getHttpServer())
+      .post('/hook')
+      .set({
+        'x-github-event': 'installation',
+        'x-hub-signature-256': `sha256=${signature}`,
+        'Content-Type': 'application/json',
+      })
+      .send(JSON.stringify(payload))
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('should return 400 without x-github-event header', async () => {
+    const webhookSecret = await testModule.app
+      .get(GithubConfig)
+      .getSecret(secretName);
+    const payload = {
+      action: 'opened',
+      issue: { number: 1 },
+      repository: {
+        id: 123,
+        name: 'Hello-World',
+        full_name: 'octocat/Hello-World',
+      },
+      sender: { login: 'octocat' },
+    };
+    const signature = createHmac('sha256', webhookSecret.secret)
+      .update(JSON.stringify(payload))
+      .digest('hex');
+
+    return request(testModule.app.getHttpServer())
+      .post('/hook')
+      .set({
+        'x-github-delivery': '72d3162e-cc78-11e3-81ab-4c9367dc0958',
+        'x-hub-signature-256': `sha256=${signature}`,
+        'Content-Type': 'application/json',
+      })
+      .send(JSON.stringify(payload))
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('should return 400 without x-hub-signature-256 header', async () => {
+    const webhookSecret = await testModule.app
+      .get(GithubConfig)
+      .getSecret(secretName);
+    const payload = {
+      action: 'opened',
+      issue: { number: 1 },
+      repository: {
+        id: 123,
+        name: 'Hello-World',
+        full_name: 'octocat/Hello-World',
+      },
+      sender: { login: 'octocat' },
+    };
+    const signature = createHmac('sha256', webhookSecret.secret)
+      .update(JSON.stringify(payload))
+      .digest('hex');
+
+    return request(testModule.app.getHttpServer())
+      .post('/hook')
+      .set({
+        'x-github-delivery': '72d3162e-cc78-11e3-81ab-4c9367dc0958',
+        'x-github-event': 'installation',
+        'Content-Type': 'application/json',
+        'x-hub-signature-512': `sha256=${signature}`,
+      })
+      .send(JSON.stringify(payload))
+      .expect(HttpStatus.BAD_REQUEST);
+  });
 });
