6beb4a26c82f6e6492e76ea6c999aa47d78606d1
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2024-10-01 12:15:30 +0700

    Removed the unused code

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,15 +1,6 @@
 import crypto from 'crypto';
 
 /**
- * Computes the MD5 hash of a given input.
- * @param input - The input string to hash.
- * @returns The MD5 hash as a hexadecimal string.
- */
-export function hashMd5(input: string): string {
-  return crypto.createHash('md5').update(input, 'utf8').digest('hex');
-}
-
-/**
  * Computes HMAC SHA256 signature of a given input using a secret key.
  * @param key - The secret key for HMAC.
  * @param input - The input string to sign.
